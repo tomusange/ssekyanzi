ad004786906976debb06774b23cc15a6e081f04d
tomusange <waiswaalexx@yahoo.com>
2015-10-15 19:23:51 +0300

    try and catch

---
+++ gad.java
@@ -0,0 +1,14 @@
+
+public class andrew {
+	public static void main(String args[]) { 
+		System.out.println("WELCOME TO SSERUNKUMA ENTERPRISE");
+		System.out.println("WERE ALRIGHT");
+		showAtts();
+	}
+
+	private static void showAtts() {
+		System.out.println("HE GAT THE WHOLE WORLD");
+		System.out.println("WORK HARD PLAY HARD");
+	}
+	
+	}
